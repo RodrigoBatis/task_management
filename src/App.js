2c11a4b3c9072455e427f0be19d1dab7843c2a79
RodrigoBatis <rodrigofreirebatista@gmail.com>
2025-01-11 17:52:11 -0300

    Adicionado o componente Grid e começando a consumir o a rota Get

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import GlobalStyle from "./styles/global.js";
 import styled from "styled-components";
 import Form from "./components/Form.js";
+import Grid from "./components/Grid.js"
+import {useEffect ,useState } from "react";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
 
 const Container = styled.div`
   width: 100%;
@@ -17,11 +20,29 @@ const Container = styled.div`
 const Title = styled.h2``;
 
 function App() {
+
+    const [tasks, setTasks] = useState([]);
+    const [onEdit, setOnEdit] = useState(null);
+
+    const getTasks = async () => {
+      try {
+        const res = await axios.get("http://localhost:8080");
+        setTasks(res.data.sort((a, b) => (a.title > b.title ? 1 : -1)));
+      } catch (error) {
+        toast.error(error);
+      }
+    };
+
+    useEffect(() => {
+      getTasks();
+    }, [setTasks]);
+
     return(
       <>
         <Container>
           <Title>Gerenciador de Tarefas</Title>
           <Form/>
+          <Grid tasks={tasks}/>
         </Container>
         <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />
         <GlobalStyle/>
